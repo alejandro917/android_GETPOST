8a75bad37abff6cebe2ec949b8d0089f51e2fc09
abpbackup <abp.backup@gmail.com>
2016-05-14 09:06:26 -0500

    Se borra todos los metodos del ActivityMain menos el onCreate. Se ajusta para que extienda Activity. Se configura el activity_main XML Se adiciona la clase login.java y se extiende Activity. Se genera el metodo onCreate con generate override methods. Se genera el login.xml a partir del ID en el metodo onCreate.
    se pega una imagen logo en res/drawable para poder mostrarla 3 segundos antes de cargar la app.
    se agrega la actividad Login al manifest.
    se carga la actividad login desde el onCreate del mainActivity

---
+++ app/src/main/java/uao/cali/com/pg2/Registro.java
@@ -0,0 +1,16 @@
+package uao.cali.com.pg2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by sala03 on 14/05/2016.
+ */
+public class Registro extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
