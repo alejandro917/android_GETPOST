413d2cc8ec32d352fd281494ea18bd084c575867
abpbackup <abp.backup@gmail.com>
2016-05-14 11:19:58 -0500

    se crea el nuevo hilo para lanzar la peticion de login. se adicionan los metodos a la nueva clase HttpThread donde se pasan las variables POST

--- app/src/main/java/uao/cali/com/pg2/Login.java
+++ app/src/main/java/uao/cali/com/pg2/Login.java
@@ -2,9 +2,16 @@ package uao.cali.com.pg2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import util.WebUtilDomi;
 
 
 /**
@@ -27,5 +34,32 @@ public class Login extends Activity{
     }
 
     public void doLog(View view) {
+        HttpThread hilo = new HttpThread();
+        hilo.execute(ETusuario.getText().toString(),ETpassword.getText().toString());
+    }
+
+    //creo un nuevo hilo en segundo plano
+    public class HttpThread extends AsyncTask<String, String, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            //sobre donde se meten los parametros post
+            Uri.Builder sobre = new Uri.Builder();
+            //se adicionan las variables al sobre
+            sobre.appendQueryParameter("usuario",params[0]).appendQueryParameter("password",params[1]);
+            //se hace la peticion y se envian las variables con post
+            try {
+                String respuesta = WebUtilDomi.POSTrequest("http://192.168.173.1:8080/WebService/webresources/service/getuser", sobre);
+                return respuesta;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+        }
     }
 }
