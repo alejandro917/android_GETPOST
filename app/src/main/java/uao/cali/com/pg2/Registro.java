f6b2a916d87cc217801dfeb58ab2f8fd52a4e4a0
abpbackup <abp.backup@gmail.com>
2016-05-14 10:48:34 -0500

    se configuran los parametros por POST para pasarlos a la URL

--- app/src/main/java/uao/cali/com/pg2/Registro.java
+++ app/src/main/java/uao/cali/com/pg2/Registro.java
@@ -1,6 +1,7 @@
 package uao.cali.com.pg2;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -34,16 +35,21 @@ public class Registro extends Activity{
 
     public void doRegistro(View view) {
         HttpThread hilo = new HttpThread();
+        hilo.execute(ETnombre.getText().toString(),ETusuario.getText().toString(),ETpassword.getText().toString());
     }
 
-    public class HttpThread extends AsyncTask<Void, String, String>{
+    public class HttpThread extends AsyncTask<String, String, String>{
 
         //este metodo es necesario adicionarlo por override methods
         @Override
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(String... params) {
             //es necesario meterlo en un try catch
             try {
-                String response = WebUtilDomi.POSTrequest("http://172.16.136.27:8080/WebService/webresources/service/crear_usuario");
+                Uri.Builder sobre = new Uri.Builder();
+                //se adicionan los parametros por POST con esta linea
+                sobre.appendQueryParameter("nombre",params[0]).appendQueryParameter("usuario",params[1]).appendQueryParameter("password",params[2]);
+                //se caputara la respuesta de la API
+                String response = WebUtilDomi.POSTrequest("http://172.16.136.27:8080/WebService/webresources/service/crear_usuario", sobre);
                 return response;
             } catch (IOException e) {
                 e.printStackTrace();
