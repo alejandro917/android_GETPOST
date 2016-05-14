9119fdd7673ef26338c3b66d25475fcd928be846
abpbackup <abp.backup@gmail.com>
2016-05-14 09:29:50 -0500

    se agrega el Try Catch al metodo doInBackground y se adiciona el metodo onPostExecute donde se imprime la respuesta en el textview. se agrega el permiso de internet en el manifest

--- app/src/main/java/uao/cali/com/pg2/Registro.java
+++ app/src/main/java/uao/cali/com/pg2/Registro.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import util.WebUtilDomi;
+
 /**
  * Created by sala03 on 14/05/2016.
  */
@@ -30,9 +34,25 @@ public class Registro extends Activity{
     }
 
     public class HttpThread extends AsyncTask<Void, String, String>{
+
+        //este metodo es necesario adicionarlo por override methods
         @Override
         protected String doInBackground(Void... params) {
+            //es necesario meterlo en un try catch
+            try {
+                String response = WebUtilDomi.GETrequest("http://www.google.com");
+                return response;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return null;
+        }
+
+        //este metodo se ejecuta despues de que se recibe la respuesta
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            salida.setText(s);
         }
     }
 }
